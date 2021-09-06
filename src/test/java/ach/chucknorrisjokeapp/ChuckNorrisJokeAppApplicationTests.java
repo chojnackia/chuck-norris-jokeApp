1201d449f077cab7e9bca463e9b3e273ea483b16
chojnackia <56468528+chojnackia@users.noreply.github.com>
2021-09-06 08:58:14 +0200

    Initial commit

---
+++ src/test/java/ach/chucknorrisjokeapp/ChuckNorrisJokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ach.chucknorrisjokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
